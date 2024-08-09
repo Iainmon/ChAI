8f9d089915f2d90b0af822b47458ca8b0e9b5381
Iain Moncrief <moncrief@oregonstate.edu>
2024-08-09 10:40:23 -1000

    Fix run script

--- runtest.sh
+++ runtest.sh
@@ -12,7 +12,7 @@ do
         # export CHPL_LAUNCHER="slurm_amudprun"
         # export GASNET_SSH_SERVERS=$(scontrol show hostnames | xargs echo)
 
-        echo "Running on $num_nodes on $(scontrol show hostnames | xargs echo) with "
+        echo "Running on $num_nodes nodes with $num_images images."
 
         python3 times.py measure "./MultiLocaleInference -nl $num_nodes --numImages=$num_images --numTries=10 --printResults=false" --name "ml_test_${num_nodes}_${num_images}"
     done
